89443493db4d5fb575d92ea16d8a85664a7185cf
WHNDigi <96463847+WHNDigi@users.noreply.github.com>
2022-03-08 16:58:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,19 +9,22 @@ mode = action[2]
 speed = 30
 
 image_sprite = [pygame.image.load("jack/"+mode+" (1).png"),
-				pygame.image.load("jack/"+mode+" (2).png"),
-				pygame.image.load("jack/"+mode+" (3).png"),
-				pygame.image.load("jack/"+mode+" (4).png"),
-				pygame.image.load("jack/"+mode+" (5).png"),
-				pygame.image.load("jack/"+mode+" (6).png"),
-				pygame.image.load("jack/"+mode+" (7).png"),
-				pygame.image.load("jack/"+mode+" (8).png"),
-				pygame.image.load("jack/"+mode+" (9).png"),
-				pygame.image.load("jack/"+mode+" (10).png")]
+		pygame.image.load("jack/"+mode+" (2).png"),
+		pygame.image.load("jack/"+mode+" (3).png"),
+		pygame.image.load("jack/"+mode+" (4).png"),
+		pygame.image.load("jack/"+mode+" (5).png"),
+		pygame.image.load("jack/"+mode+" (6).png"),
+		pygame.image.load("jack/"+mode+" (7).png"),
+		pygame.image.load("jack/"+mode+" (8).png"),
+		pygame.image.load("jack/"+mode+" (9).png"),
+		pygame.image.load("jack/"+mode+" (10).png")]
 
 clock = pygame.time.Clock()
 value = 0
 for i in range(300):
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
 	clock.tick(speed)
 	if value >= len(image_sprite):
 		value = 0
@@ -30,3 +33,4 @@ for i in range(300):
 	pygame.display.update()
 	window.fill((0, 0, 0))
 	value += 1
+pygame.quit()
